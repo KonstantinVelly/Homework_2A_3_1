1d6f1b4164d3d6e29e774124528b57a7c50fd272
Konstantin Velly <konstantinvelly@gmail.com>
2022-09-11 11:43:54 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+      // Given - When - Then
+      // Предусловия
+      given()
+          .baseUri("http://localhost:9999/api/v1")
+      // Выполняемые действия
+      .when()
+          .get("/demo/accounts")
+      // Проверки
+      .then()
+          .statusCode(200)
+        // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+          .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
+}
